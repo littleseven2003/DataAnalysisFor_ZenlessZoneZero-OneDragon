068ea5ca187aba87b0ab11ded662d5820a7e3e29
Littleseven <littleseven2003@gmail.com>
2024-12-11 16:57:18 +0800

    增加功能：克隆远端仓库到本地

--- GetData.py
+++ GetData.py
@@ -6,6 +6,24 @@ Author: 筱柒_littleseven
 Github: https://github.com/littleseven2003
 """
 
+import git
+import os
+import pandas as pd
+
+class RepoManager:
+    def __init__(self, repo_url, local_path='./repo'):
+        self.repo_url = repo_url
+        self.local_path = local_path
+
+    def clone_repo(self):
+        # 检查仓库是否已经存在
+        if not os.path.exists(self.local_path):
+            print(f"Cloning repository from {self.repo_url}...")
+            git.Repo.clone_from(self.repo_url, self.local_path)
+        else:
+            print("Repository already exists, no need to clone.")
+
+
 if __name__ == "__main__":
     # Your main code logic goes here
     print("Hello from GetData.py!")
