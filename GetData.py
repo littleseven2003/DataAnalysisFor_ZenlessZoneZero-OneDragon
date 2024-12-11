6dec992c2e4d302c89b4c5b3e68b304bf89f07e1
Littleseven <littleseven2003@gmail.com>
2024-12-11 16:49:11 +0800

    初始化GetData.py文件(用于从本地仓库或Github获取信息)

---
+++ GetData.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+GetData.py
+Author: 筱柒_littleseven
+Github: https://github.com/littleseven2003
+"""
+
+if __name__ == "__main__":
+    # Your main code logic goes here
+    print("Hello from GetData.py!")
+
+    # Example usage:
+    # You can call functions from other modules here
+    # from my_module import my_function
+    # my_function()
